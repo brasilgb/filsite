e09fdc2d004f4b1b1bbd79b01f5d786645018dfa
Anderson Brasil <andersonbrasil72@gmail.com>
2025-04-16 13:37:45 -0300

    Push

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {!! SEO::generate() !!}
 
-    <title>{{ $title ?? 'Site' }}</title>
+    {{-- <title>{{ $title ?? 'Site' }}</title> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
