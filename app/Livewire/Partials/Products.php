47aee2938da4a03d2749d89d173b967689dd6082
Anderson .Brasil <andersonbrasil72@gmail>
2025-04-22 08:53:34 -0300

    Push

--- app/Livewire/Partials/Products.php
+++ app/Livewire/Partials/Products.php
@@ -3,10 +3,17 @@
 namespace App\Livewire\Partials;
 
 use App\Models\Product;
+use App\Models\Setting;
 use Livewire\Component;
 
 class Products extends Component
 {
+    public $numwhats;
+
+    public function mount(){
+        $momeSEO = Setting::first();
+        $this->numwhats = is_null($momeSEO) ? '' : $momeSEO->whatsapp;
+    }
     public function render()
     {
         $products = Product::where('active', 1)->where('home', 1)->get();
